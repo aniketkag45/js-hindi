bcaad1a31bff9114682932d9cf50db242991645c
aniketkag45 <162972842+aniketkag45@users.noreply.github.com>
2025-03-01 10:48:50 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId= 144553
+let accountEmail="aniketkag07@gmail.com"
+var accountPassward ="12345"
+accountCity="jaipur"
+
+let accountState
+
+accountEmail="dbhfsavdgjv@dfvgh"
+accountPassward="2121121"
+accountCity="Bangluru"
+
+console.table([accountId,accountEmail,accountPassward,accountCity,accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,6 @@
+"use strict";//treat all js code as newew version
+alert(3+3)//we are using node.js not browser
+
+let name="aniket"
+let age=21
+let isLogin=false
