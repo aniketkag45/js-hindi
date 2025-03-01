40da9ce0eacce51929b6696e400200d028e4d113
aniketkag45 <162972842+aniketkag45@users.noreply.github.com>
2025-03-01 16:14:30 +0000

    container config done

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,17 @@
+let score="33abc"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber=Number(score)
+//console.log(typeof valueInNumber);
+//console.log( valueInNumber);
+
+
+let isLoggeddIn = 1
+let booleanIsLoggedIn = Boolean(isLoggeddIn)
+//console.log( booleanIsLoggedIn);
+
+let value=3
+let negValue= -value
+console.log(negValue);
